0c81108605ce56cfd4bb6e72e53aacec6ada65ea
節原有貴 <fushihara@fushiharayukinoMacBook-Air.local>
2020-03-19 09:48:54 +0900

    CircleCIエラー修正5

--- db/migrate/20200205043353_devise_token_auth_create_users.rb
+++ db/migrate/20200205043353_devise_token_auth_create_users.rb
@@ -17,11 +17,11 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## trackable/サインイン回数・時刻・IPアドレスを保存
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
 
       ## Confirmable/メール送信による登録確認
       t.string   :confirmation_token
